fd8d29e028f0402198bd8b7acadbd06464376033
Tatsuto YAMAMOTO <me@laminne33569.net>
2023-12-13 14:17:39 +0900

    fix: フォーマット

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,15 @@ KCMS - Matz葉ガニロボコン 大会運営支援ツール
 MIT License.
 */
 import { Hono } from "hono";
-import {entryHandler} from "./entry/main.js";
-import {serve} from "@hono/node-server";
-
+import { entryHandler } from "./entry/main.js";
+import { serve } from "@hono/node-server";
 
 const app = new Hono();
 
 app.route("/entry", entryHandler);
 
 serve(app, () => {
-    console.log("kcms\n(C) 2023 Poporon Network & Other Contributors\nMIT License.\nServer started: 3000");
-})
+  console.log(
+    "kcms\n(C) 2023 Poporon Network & Other Contributors\nMIT License.\nServer started: 3000",
+  );
+});
