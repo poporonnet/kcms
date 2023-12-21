79f6a99d7ab530ec24c54481700e048d80e7285e
C4NoN424 <c4non424@gmail.com>
2023-12-21 14:17:37 +0900

    fix: error.tsとerror.test.tsのフォーマットを修正

--- src/entry/adaptor/errors.test.ts
+++ src/entry/adaptor/errors.test.ts
@@ -3,14 +3,14 @@
 // the error code of the conversion destination is written in README.md
 
 function errorToCode(error: Error): string {
-    switch (error.message) {
-        case "too many members":
-            return "TOO_MANY_MEMBERS";
-        case "no member":
-            return "NO_MEMBER";
-        case "teamName Exists":
-            return "TEAM_ALREADY_EXISTS";
-        default:
-            return "UNKNOWN_ERROR";
-    }
+  switch (error.message) {
+    case "too many members":
+      return "TOO_MANY_MEMBERS";
+    case "no member":
+      return "NO_MEMBER";
+    case "teamName Exists":
+      return "TEAM_ALREADY_EXISTS";
+    default:
+      return "UNKNOWN_ERROR";
+  }
 }
